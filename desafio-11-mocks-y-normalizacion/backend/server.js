5dd48a41dbd0c456800d8ff6f3c12a7583cc41d5
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-02-06 19:48:23 -0300

    comienzo desafio 11

---
+++ desafio-11-mocks-y-normalizacion/backend/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import http from 'http';
+import { Server as SocketServer } from 'socket.io';
+import { Container } from './controllers/dbContainer.js';
+import { mariadbConfig } from './config/config.js';
+import router from './router/index.js';
+import { Messages } from './controllers/messagesContainer.js';
+import { connect } from 'mongoose';
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+	cors: {
+		origin: 'http://localhost:3000'
+	}
+});
+const PORT = process.env.PORT;
+
+const controlProducts = new Container(mariadbConfig, 'products');
+const controlMessages = new Messages();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.use('/api', router);
+
+io.on('connection', async socket => {
+	console.log('A client has connected');
+	const dbProds = await controlProducts.getAll();
+	io.sockets.emit('products', dbProds);
+	const dbMess = await controlMessages.getAll();
+	io.sockets.emit('messages', dbMess);
+
+	socket.on('newProduct', newProduct => {
+		controlProducts.save(newProduct);
+		const dbProds = controlProducts.getAll();
+		io.sockets.emit('products', dbProds);
+	});
+
+	socket.on('newMessage', async newMessage => {
+		await controlMessages.save(newMessage);
+		const dbMess = await controlMessages.getAll();
+		io.sockets.emit('messages', dbMess);
+	});
+
+});
+
+server.listen(PORT, async () => {
+	await connect(process.env.MONGO)
+    console.log(`Server listening on port ${PORT}`);
+})
+
+server.on('error', err => {
+    console.log(err);
+});
